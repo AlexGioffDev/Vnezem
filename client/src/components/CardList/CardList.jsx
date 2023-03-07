f22952cdb0a1e3669929a17f226752f62e322db1
Alex <alex.gioffre_dev@outlook.it>
2023-03-07 18:30:36 +0100

    Change name file CardList in to CardLists

--- client/src/components/CardList/CardList.jsx
+++ client/src/components/CardList/CardList.jsx
@@ -1,18 +1,22 @@
-import Card from '../Card/Card'
-import styles from './CardList.module.css'
+import Card from "../Card/Card";
+import styles from "./CardList.module.css";
 
+const CardLists = ({ movies }) => {
+  return (
+    <div className={styles.cardGrid}>
+      {movies.map((movie) => {
+        return (
+          <Card
+            key={movie.id}
+            id={movie.id}
+            title={movie.title}
+            original_title={movie.original_title}
+            poster={movie.poster_path}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
-const CardList = ({movies}) => {
-    return (
-        <div className={styles.cardGrid}>
-            {movies.map((movie) => {
-                return (
-                    <Card key={movie.id} id={movie.id} title={movie.title} original_title={movie.original_title} poster={movie.poster_path} />
-                )
-            })}
-           
-        </div>
-    )
-}
-
-export default CardList
+export default CardLists;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,51 +1,45 @@
-import { useQuery, gql } from '@apollo/client';
-import { useContext } from 'react';
-import { Navigate } from 'react-router-dom';
-import Navbar from '../components/Navbar/Navbar';
-import { StoreContext } from '../store/storeContentex';
-import CardList from '../components/CardList/CardList';
-
-
+import { useQuery, gql } from "@apollo/client";
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import Navbar from "../components/Navbar/Navbar";
+import { StoreContext } from "../store/storeContentex";
+import CardLists from "../components/CardList/CardList";
 
 const UPCOMING_MOVIES_QUERY = gql`
-    query {
-        moviesUpcoming {
-            movies {
-                title
-                original_title
-                id
-                poster_path
-            }
-            maxDate
-        }
+  query {
+    moviesUpcoming {
+      movies {
+        title
+        original_title
+        id
+        poster_path
+      }
+      maxDate
     }
-`
-
-
+  }
+`;
 
 const Home = () => {
+  const {
+    state: { user },
+  } = useContext(StoreContext);
+  const { data, loading, error } = useQuery(UPCOMING_MOVIES_QUERY);
 
-    const {
-        state: { user }
-    } = useContext(StoreContext);
-    const { data, loading, error } = useQuery(UPCOMING_MOVIES_QUERY);
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
 
-    if (!user) {
-        return <Navigate to="/login" />
-    }
+  if (loading) return "Loading...";
+  console.log({ error });
+  if (error) return <pre>{error.message}</pre>;
 
-    if (loading) return "Loading...";
-    console.log({error})
-    if (error) return <pre>{error.message}</pre>
-
-
-    const movies = data.moviesUpcoming.movies
-    return (
-        <>
-            <Navbar />
-            <CardList movies={movies} />
-        </>
-    )
-}
+  const movies = data.moviesUpcoming.movies;
+  return (
+    <>
+      <Navbar />
+      <CardLists movies={movies} />
+    </>
+  );
+};
 
 export default Home;
